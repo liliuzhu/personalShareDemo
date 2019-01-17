05743ea04e0618208c7538541e54f20043eeabb2
李留住 <liliuzhu@renrenche.com>
2019-01-17 20:12:32 +0800

    feat: up

--- WebSocket/webSocket/server/socket.config.js
+++ WebSocket/webSocket/server/socket.config.js
@@ -1,10 +1,13 @@
 // websocket.js
 const ws = require('nodejs-websocket')
+let users = [] //保存所有在线用户的昵称
 
-const createServer = () => {
+const createServer = (port) => {
   let server = ws.createServer(connection => {
     connection.on('text', function(result) {
-      console.log('发送消息', result)
+      console.log('收到消息', result)
+      console.log(server.connections.length)
+      messageHandle(server,connection, result)
     })
     connection.on('connect', function(code) {
       console.log('开启连接', code)
@@ -13,10 +16,50 @@ const createServer = () => {
       console.log('关闭连接', code)
     })
     connection.on('error', function(code) {
+      // 某些情况如果客户端多次触发连接关闭，会导致connection.close()出现异常，这里try/catch一下
+      try {
+        connection.close()
+      } catch (error) {
+        console.log('close异常', error)
+      }
       console.log('异常关闭', code)
     })
+  }).listen(port, () => {
+    console.log('打开浏览器并访问 http://localhost:' +port)
   })
   return server
 }
+const messageHandle = (server, conn, msg) => {
+  const obj = JSON.parse(msg)
+  if (obj.action === 'login') {
+    if (users.indexOf(obj.data.nickname) > -1) {
+      const params = {}
+      broadcast.self(server, conn, params)
+    } else {
 
-module.exports = createServer()
+    }
+  } else {
+
+  }
+  // broadcast(server, conn, msg)
+}
+const broadcast = {
+  all: (server, conn, msg) => {
+    server.connections.forEach(function (connection) {
+      connection.sendText(msg)
+    })
+  },
+  self: (server, conn, msg) => {
+    conn.sendText(msg)
+  },
+  excludeSele: (server, conn, msg) => {
+    server.connections.forEach(function (connection) {
+      if (conn !== connection) {
+        connection.sendText(msg)
+      }
+    })
+  }
+
+}
+
+module.exports = createServer
--- WebSocket/webSocket/src/plugins/socketClient/index.js
+++ WebSocket/webSocket/src/plugins/socketClient/index.js
@@ -1,43 +1,40 @@
-import io from 'socket.io-client'
+// import io from 'socket.io-client'
 
-let wsUrl = 'http://172.18.2.17:3000'
-const CLIENT = { // eslint-disable-line
-  socket: null,
-  init(username) {
-    // 连接websocket后端服务器
-    this.socket = io.connect(wsUrl, {'force new connection': true})
-    // this.socket.on('open', _ => {
-    //   console.log('open')
-    // })
-    this.socket.on('connect', _ => {
-      console.log('connect')
-      this.socket.emit('login', username)
-    })
-    this.socket.on('newMsg', (user, msg, color) => {
-      this._displayNewMsg(user, msg, color)
-    })
-    // 在前端接收到这个事件后我们显示一条信息通知用户。
-    this.socket.on('nickExisted', _ => {
-      // document.getElementById('info').textContent = '称被占用' // 显示昵称被占用的提示
-    })
-    // 通知前端登陆成功，前端接收到这个成功消息后将灰色遮罩层移除显示聊天界面。
-    this.socket.on('loginSuccess', _ => {
-      console.log('loginSuccess')
-      // document.title = 'hichat | ' + document.getElementById('nicknameInput').value
-      // document.getElementById('loginWrapper').style.display = 'none';//隐藏遮罩层显聊天界面
-      // document.getElementById('messageInput').focus();//让消息输入框获得焦点
-    })
-    this.socket.on('system', (nickName, userCount, type) => {
-      var msg = nickName + (type === 'login' ? ' 加入' : '离开')
-      // 指定系统消息显示为红色
-      this._displayNewMsg('system ', msg, 'red')
-      // document.getElementById('status').textContent = userCount + (userCount > 1 ? ' users' : ' user') + ' online';
-    })
-  },
-  postMsg(msg, color) {
-    this.socket.emit('postMsg', msg, color)
-  }
-}
+let wsUrl = 'ws://172.18.4.143:3000'
+// const CLIENT = { // eslint-disable-line
+//   socket: null,
+//   init(username) {
+//     // 连接websocket后端服务器
+//     this.socket = io.connect(wsUrl, {'force new connection': true})
+//     // this.socket.on('open', _ => {
+//     //   console.log('open')
+//     // })
+//     this.socket.on('connect', _ => {
+//       console.log('connect')
+//       this.socket.emit('login', username)
+//     })
+//     this.socket.on('newMsg', (user, msg, color) => {
+//       this._displayNewMsg(user, msg, color)
+//     })
+//     // 在前端接收到这个事件后我们显示一条信息通知用户。
+//     this.socket.on('nickExisted', _ => {
+//       // document.getElementById('info').textContent = '称被占用' // 显示昵称被占用的提示
+//     })
+//     // 通知前端登陆成功，前端接收到这个成功消息后将灰色遮罩层移除显示聊天界面。
+//     this.socket.on('loginSuccess', _ => {
+//       console.log('loginSuccess')
+//     })
+//     this.socket.on('system', (nickName, userCount, type) => {
+//       var msg = nickName + (type === 'login' ? ' 加入' : '离开')
+//       // 指定系统消息显示为红色
+//       this._displayNewMsg('system ', msg, 'red')
+//       // document.getElementById('status').textContent = userCount + (userCount > 1 ? ' users' : ' user') + ' online';
+//     })
+//   },
+//   postMsg(msg, color) {
+//     this.socket.emit('postMsg', msg, color)
+//   }
+// }
 
 class Client {
   constructor(url, bus) {
@@ -46,19 +43,27 @@ class Client {
     this._socket = null
   }
   init(events) {
-    this._socket = io.connect(this._wsUrl, {'force new connection': true})
-    for (let i = 0, len = events.length; i < len; i++) {
-      this._socket.on(events[i], (...arg) => {
-        console.log(1, events[i])
-        this._bus.$emit(events[i], ...arg)
-      })
+    if ('WebSocket' in window) {
+      this._socket = new WebSocket(wsUrl)
+      // for (let i = 0, len = events.length; i < len; i++) {
+      //   this._socket.addEventListener(events[i], (...arg) => {
+      //     console.log(1, events[i])
+      //     this._bus.$emit(events[i], ...arg)
+      //   })
+      // }
+      this._socket.onopen = () => {
+        console.log(1234)
+        this.sendMsgToServer({action: '123', msg: '456'})
+      }
+    } else {
+      alert('您的浏览器不支持 WebSocket!')
     }
   }
-  postMsg(action, msg, color) {
-    this._socket.emit(action, msg, color)
-  }
-  sendMsgToServer(...arg) {
-    this._socket.emit(...arg)
+  // postMsg(action, msg, color) {
+  //   this._socket.send(action, msg, color)
+  // }
+  sendMsgToServer(arg) {
+    this._socket.send(JSON.stringify(arg))
   }
 }
 
